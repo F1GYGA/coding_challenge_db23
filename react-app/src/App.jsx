5a243f2ee0f8c664bfceac6e1a078e944de4e762
chao <chaoaskafan@gmail.com>
2023-08-03 13:07:38 +0100

    log in page

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,9 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import AllBonds from "./components/AllBonds";
-
+import SignIn from './components/SignIn';
 const App = () => {
-  return <AllBonds />;
+  return <SignIn />;
 };
 
 export default App;
