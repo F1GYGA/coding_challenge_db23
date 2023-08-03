238fc06a64969122a460b38a3c436686a96ed62a
chao <chaoaskafan@gmail.com>
2023-08-03 11:50:18 +0100

    worked on front end need fixing on table

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
-import { Pets } from "./components/pets/Pets";
+import AllBonds from "./components/AllBonds";
 
 const App = () => {
-  return <Pets />;
+  return <AllBonds />;
 };
 
 export default App;
