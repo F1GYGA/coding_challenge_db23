6a990f94137928dbac81d0d145bd5e893b7dcc7c
vlados2000 <vlad.elev14@gmail.com>
2023-08-03 14:12:36 +0100

    updated maturity 5

--- java-api/src/main/java/com/db/grad/javaapi/controller/BondController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/BondController.java
@@ -2,19 +2,12 @@ package com.db.grad.javaapi.controller;
 
 
 import com.db.grad.javaapi.dto.BondDto;
-import com.db.grad.javaapi.exception.ResourceNotFoundException;
 import com.db.grad.javaapi.model.Bond;
 import com.db.grad.javaapi.service.BondService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.swing.text.DateFormatter;
-import java.time.Duration;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import static java.lang.Math.abs;
@@ -38,21 +31,13 @@ public class BondController {
     }
 
 
-    @GetMapping("/bonds/maturity/{date}")
-    public List<BondDto> getMaturityBonds(@PathVariable(value = "date") String date) {
+    @GetMapping("/bonds/maturity")
+    public List<BondDto> getMaturityBonds(@RequestParam String date) {
         List<BondDto> result = new ArrayList<>();
 
-        for (Bond bond : bondService.getAllBonds()) {
-            String maturityDate = String.valueOf(bond.getMaturityDate());
-
-            LocalDate date1 = LocalDate.parse(date);
-            LocalDate date2 = LocalDate.parse(maturityDate);
-
-            long daysBetween = abs(date1.toEpochDay() - date2.toEpochDay());
-            if (daysBetween <= 5)
-                result.add(new BondDto(bond.getIsin(), bond.getCusip(), bond.getBondCurrency(), bond.getCouponPercent(),
-                        bond.getFaceValue(), bond.getIssuerName(), bond.getStatus(), bond.getType(), bond.getMaturityDate()));
-        }
+        for (Bond bond : bondService.getMaturityBonds(date))
+            result.add(new BondDto(bond.getIsin(), bond.getCusip(), bond.getBondCurrency(), bond.getCouponPercent(),
+                    bond.getFaceValue(), bond.getIssuerName(), bond.getStatus(), bond.getType(), bond.getMaturityDate()));
 
         return result;
     }
