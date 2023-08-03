b474c4b8917b6d0aaa18af96af624fe29b0da57e
Ruslan Salkaev <rsalkaev@gmail.com>
2023-08-03 13:44:52 +0100

    Show user books

--- java-api/src/main/java/com/db/grad/javaapi/controller/BondController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/BondController.java
@@ -4,13 +4,11 @@ package com.db.grad.javaapi.controller;
 import com.db.grad.javaapi.dto.BondDto;
 import com.db.grad.javaapi.model.Bond;
 import com.db.grad.javaapi.model.CounterParty;
-import com.db.grad.javaapi.model.Trade;
 import com.db.grad.javaapi.service.BondService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins="http://localhost:3000")
@@ -24,7 +22,7 @@ public class BondController {
         List<BondDto> list = new ArrayList<>();
 
         for (Bond bond : bondService.getAllBonds()) {
-            List<CounterParty> holders = bond.getTrades().stream().map(Trade::getCounterparty).distinct().collect(Collectors.toList());
+            List<CounterParty> holders = bondService.getHolders(bond);
             list.add(new BondDto(bond.getIsin(), bond.getCusip(), bond.getBondCurrency(), bond.getCouponPercent(),
                     bond.getFaceValue(), bond.getIssuerName(), bond.getStatus(), bond.getType(), bond.getMaturityDate(), holders));
         }
--- java-api/src/main/java/com/db/grad/javaapi/controller/BookController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/BookController.java
@@ -1,9 +1,14 @@
 package com.db.grad.javaapi.controller;
 
+import com.db.grad.javaapi.dto.BondDto;
 import com.db.grad.javaapi.dto.BookDto;
+import com.db.grad.javaapi.model.Bond;
 import com.db.grad.javaapi.model.Book;
+import com.db.grad.javaapi.model.CounterParty;
+import com.db.grad.javaapi.service.BondService;
 import com.db.grad.javaapi.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +23,26 @@ public class BookController {
     @Autowired
     BookService bookService;
 
-    @GetMapping("/books")
-    public List<BookDto> getBooks() {
-        List<BookDto> list = new ArrayList<>();
+    @Autowired
+    BondService bondService;
 
-        for (Book book : bookService.getAllBooks()) {
-            list.add(new BookDto(book.getId(), book.getBookName()));
+    @GetMapping("/books")
+
+    public List<BookDto> getUserBooks() {
+
+        List<BookDto> list = new ArrayList<>();
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        List<Book> userBooks = bookService.getUserBooks(username);
+
+        for (Book book : userBooks) {
+            List<Bond> bondsInBook = bondService.getBondsInBook(book);
+            List<BondDto> bondDtoList = new ArrayList<>();
+            for (Bond bond: bondsInBook) {
+                List<CounterParty> holders = bondService.getHolders(bond);
+                bondDtoList.add(new BondDto(bond.getIsin(), bond.getCusip(), bond.getBondCurrency(), bond.getCouponPercent(),
+                        bond.getFaceValue(), bond.getIssuerName(), bond.getStatus(), bond.getType(), bond.getMaturityDate(), holders));
+            }
+            list.add(new BookDto(book.getId(), book.getBookName(), bondDtoList));
         }
 
         return list;
