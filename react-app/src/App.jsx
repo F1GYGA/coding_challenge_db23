c2d5062ed371db9735a53185b65e5f7e31838f50
Chao <chaoaskafan@gmail.com>
2023-08-04 15:18:51 +0100

    implementing navbar

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -5,24 +5,41 @@ import LogIn from './components/LogIn';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Trades from './components/Trades'
-
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
 
-    <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/login" exact element={<LogIn/>} />
-      <Route path="/main" exact element={<AllBonds/>} />
-      <Route path="/"  exact element={<LogIn/>} />
-      <Route path="/trades" exact element={<Trades/>}></Route>
-     // <Route path="/trades" exact element={<Trades/>}></Route>
-    </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+    <>
+      <Navbar expand='lg' className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">Sign In</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/main">All Bonds</Nav.Link>
+              <Nav.Link href="/main">Maturing Bonds</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-  
+
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" exact element={<LogIn />} />
+            <Route path="/main" exact element={<AllBonds />} />
+            <Route path="/" exact element={<LogIn />} />
+            <Route path="/trades" exact element={<Trades />}></Route>
+     // <Route path="/trades" exact element={<Trades />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
+
+    </>
   );
 };
 
