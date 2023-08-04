efbc6fcd41ef65834018f1d36f2fcf3802b5302c
Ruslan Salkaev <rsalkaev@gmail.com>
2023-08-04 13:31:53 +0100

    Fix show all bonds@

--- react-app/src/components/AllBonds.jsx
+++ react-app/src/components/AllBonds.jsx
@@ -3,16 +3,7 @@ import BondsDetail from './BondsDetail'
 import { useEffect } from 'react';
 import { getAllBonds } from '../services/service';
 
-const bonds = [
-  {
-    'isin': 'A12356111', 'cusip': '123456bh0', 'currency': 'USD', 'coupon_percent': 2, 'face_value': 900, 'issuer_name': 'UBS Facebook (USD)', 'status': 'active',
-    'type': 'CORP', 'maturity': '2021-09-30'
-  },
-  {
-    'isin': 'A12356122', 'cusip': '123456xh0', 'currency': 'CNY', 'coupon_percent': 20, 'face_value': 9000, 'issuer_name': 'CS Facebook (CNY)', 'status': 'active',
-    'type': 'CORP', 'maturity': '2021-09-22'
-  }
-]
+const bonds = []
 
 const trades = [
   {
@@ -47,29 +38,29 @@ console.log(JSON.stringify(mergedArr2));
 //inbetween is the temporary code to combine tables and test the ui, should not be used once the backend is functioning
 
 const AllBonds = () => {
-//   const [bonds, setBonds] = useState([]);
+  const [bonds, setBonds] = useState([]);
   
-//   useEffect(() => {
-//     getBondsFromAPI();
-//   },[]);
+  useEffect(() => {
+    getBondsFromAPI();
+  },[]);
 
-//   const getBondsFromAPI = () => {
-//     getAllBonds()
-//       .then(res => {
-//         setBonds(res.data);
-//       })
-//       .catch(err => {
-//         setBonds([]);
-//         console.log(err);
-//       })
-//   }
+  const getBondsFromAPI = () => {
+    getAllBonds()
+      .then(res => {
+        setBonds(res.data);
+      })
+      .catch(err => {
+        setBonds([]);
+        console.log(err);
+      })
+  }
 
 
 
 
   return (
-    //<BondsDetail info={bonds} />
-    <BondsDetail info={mergedArr2} />
+    <BondsDetail info={bonds} />
+    //<BondsDetail info={mergedArr2} />
   )
 }
 
