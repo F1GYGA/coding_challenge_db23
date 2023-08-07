2e8d947dff4fa79693d9e43466235e6cac40d003
Chao <chaoaskafan@gmail.com>
2023-08-07 11:37:14 +0100

    implemented due-today alert

--- react-app/src/components/CollapsableTable/CollapsableTable.jsx
+++ react-app/src/components/CollapsableTable/CollapsableTable.jsx
@@ -1,29 +1,36 @@
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import Row from "../Row";
-
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const CollapsableTable = ({rows}) => {
+    const [date, setDate] = useState(new Date());
     return (
-        <TableContainer component={Paper}
-        // 
-        >
-            <Table aria-label="collapsible table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>ISIN</TableCell>
-                        <TableCell align="center">Maturity Date</TableCell>
-                        <TableCell align="center">CUSIP</TableCell>
-                        <TableCell align="center">Issuer</TableCell>
-                        <TableCell />
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <Row key={row.name} row={row} />
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <>
+            <DatePicker selected={date} onChange={(startDate) => setDate(startDate)} />
+            <TableContainer component={Paper}
+            // 
+            >
+                <Table aria-label="collapsible table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>ISIN</TableCell>
+                            <TableCell align="center">Maturity Date</TableCell>
+                            <TableCell align="center">CUSIP</TableCell>
+                            <TableCell align="center">Issuer</TableCell>
+                            <TableCell />
+                            <TableCell />
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <Row key={row.name} row={row} day={date} />
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
     );
 }
 
