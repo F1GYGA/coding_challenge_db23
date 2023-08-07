f9e9c5dcb3875e139102c6d8888c0c0a1cc7af92
Ruslan Salkaev <rsalkaev@gmail.com>
2023-08-07 10:58:17 +0100

    Show clients frontend

---
+++ react-app/src/components/Clients.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import BondsDetail from './BondsDetail'
+import { useEffect } from 'react';
+import { getCounterParties } from '../services/service';
+import { Collapse } from 'antd';
+  
+const { Panel } = Collapse;
+
+const Clients = () => {
+    const [clients, setClients] = useState([]);
+
+    useEffect(() => {
+        getClientsFromAPI();
+    }, []);
+
+    const getClientsFromAPI = () => {
+        getCounterParties()
+            .then(res => {
+                setClients(res.data);
+                console.log(res.data);
+            })
+            .catch(err => {
+                setClients([]);
+                console.log(err);
+            })
+    }
+
+
+
+
+    return (
+        <Collapse>
+            {clients.map((client) => (
+                <Panel header={client.holderName} key={client.id}>
+                    <BondsDetail info={client.bonds} />
+                </Panel>
+            ))}
+        </Collapse>
+    )
+}
+
+
+export default Clients;
